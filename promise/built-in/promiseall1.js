3090d1a1199763c28d6f478a4177a208567c7924
PAVAN R <82048445+Pavan5959@users.noreply.github.com>
2021-06-15 21:04:46 +0530

    Create promiseall1.js

---
+++ promise/built-in/promiseall1.js
@@ -0,0 +1,21 @@
+/*both resolves */
+let promise1 = new Promise(function(resolve, reject){
+  setTimeout(function(){
+      resolve('resolved');
+  }, 2000)
+})
+
+let promise2 = new Promise(function(resolve, reject){
+  setTimeout(function(){
+    resolve('rejected');
+  },4000)
+})
+
+Promise.all([promise1, promise2])
+       .then(function(data){
+  console.log(data);
+}) .catch(function(error){
+  console.log(error);
+  
+})
+//["resolved", "rejected"]
