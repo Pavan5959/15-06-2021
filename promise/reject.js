0ffdcba5c26a2589336bc2018f303e92dcc08c7b
PAVAN R <82048445+Pavan5959@users.noreply.github.com>
2021-06-15 16:07:55 +0530

    Create reject.js

---
+++ promise/reject.js
@@ -0,0 +1,13 @@
+let promise = new Promise(function(resolve, reject){
+  setTimeout(function(){
+    reject('done!');
+  },2000)
+})
+
+promise.then(function(data){
+  console.log(data);
+}, function(error){
+  console.log('error ' + error );
+})
+
+console.log("promise")
