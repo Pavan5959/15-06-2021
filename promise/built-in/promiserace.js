cc98b1bb6a6ee92c699a3215022311de8d699cfb
PAVAN R <82048445+Pavan5959@users.noreply.github.com>
2021-06-15 21:06:16 +0530

    Create promiserace.js

---
+++ promise/built-in/promiserace.js
@@ -0,0 +1,20 @@
+/*first promise checks*/
+let promise1 = new Promise(function(resolve, reject){
+  setTimeout(function(){
+      resolve('resolved');
+  }, 2000)
+})
+
+let promise2 = new Promise(function(resolve, reject){
+  setTimeout(function(){
+    resolve('rejected');
+  },2000)
+})
+
+Promise.race([promise1, promise2])
+       .then(function(data){
+  console.log(data);
+}) .catch(function(error){
+  console.log(error);
+  
+})
