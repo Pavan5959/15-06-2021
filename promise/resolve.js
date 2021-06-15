8d2f7958c0d358959371b39d5c14468613b35e29
PAVAN R <82048445+Pavan5959@users.noreply.github.com>
2021-06-15 16:04:37 +0530

    Create resolve.js

---
+++ promise/resolve.js
@@ -0,0 +1,11 @@
+let promise = new Promise(function(resolve, reject){
+  setTimeout(function(){
+    resolve('done!');
+  },2000)
+})
+
+promise.then(function(data){
+  console.log(data);
+})
+
+console.log("promise")
